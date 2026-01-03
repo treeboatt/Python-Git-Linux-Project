254242ae59253fa4b27b7368d17f7e071a1e5bfa
Maxime GRUEZ <maxime.gruez@gmail.com>
2026-01-03 17:03:29 +0100

    fix: patch final dashboard fonctionnel

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,9 +3,14 @@ import pandas as pd
 
 def fetch_data(ticker, period="5d", interval="5m"):
     try:
-        df = yf.download(ticker, period=period, interval=interval, progress=False)
+        df = yf.download(ticker, period=period, interval=interval, progress=False, auto_adjust=True)
         
-        if df.empty:
+        if isinstance(df.columns, pd.MultiIndex):
+            df.columns = df.columns.get_level_values(0)
+
+        df = df.loc[:, ~df.columns.duplicated()]
+
+        if df.empty or 'Close' not in df.columns:
             return pd.DataFrame()
             
         return df
