155e875a24adad788bc2b72e435a63045eaa0b7f
Matthieu Hanna Gerguis <matthieu.hannagerguis@gmail.com>
2026-01-09 03:14:35 +0100

    fix: add stub quant_a to prevent import crash on quant-b branch

---
+++ src/quant_a.py
@@ -0,0 +1,4 @@
+﻿import streamlit as st
+
+def render_quant_a(show_header: bool = True):
+    st.info("📈 Quant A n'est pas dans cette branche. Passe sur `quant-a-module` pour le module Single Asset.")
